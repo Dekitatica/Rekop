2959eaafe881653b03c4c7eb03f5f755754d24f4
bob@example.com <bob@example.com>
2024-07-29 12:52:16 +0200

    Neke Promene

--- server.py
+++ server.py
@@ -1,14 +1,36 @@
 import socket
 import threading
 
-def handle_client(con):
+
+class Player:
+    def __init__(self) -> None:
+        money = 0
+        x = 0
+        y = 0
+
+class Client:
+    def __init__(self,player : Player,con : socket.socket) -> None:
+        self.player = player
+        self.con = con
+
+def request_move(cli,args) -> None:
+    pass
+
+def handle_client(cli : Client) -> None:
+    con = cli.con
     while True:
         try:
             data = con.recv(1024)
             data = data.decode()
+            data = str(data)
             if data!="":
                 print(data)
+                if data.startswith("request_move"):
+                    request_move(data.split(":")[1])
+
+
         except Exception as e:
+            print(e)
             pass
 
 available_ids = [1]*100
@@ -35,7 +57,10 @@ while True:
     client_socket, client_address = server_socket.accept()
     print(f"[*] Accepted connection from {client_address[0]}:{client_address[1]}")
 
-    connections.append(client_socket)
+    new_client = Client(Player(),client_socket)
+
+
+    connections.append(new_client)
     t1 = threading.Thread(target=handle_client(connections[-1]))
     t1.start()
     threads.append(t1)
