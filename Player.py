e57bbd7fb3532f7b227f63e050d31ac3b129c975
Deki Tata <dekitempmail@gmail.com>
2024-07-30 12:53:18 +0200

    removed grass for fps boost , collision works

--- Player.py
+++ Player.py
@@ -12,22 +12,67 @@ class Player:
         self.txt_player_left = pygame.transform.scale(self.txt_player_left , (45 , 65))
         self.txt_player_right = pygame.transform.scale(self.txt_player_right , (45 , 65))
         self.picture = self.txt_player_right
+        self.collision = False
+        self.rect = pygame.Rect(self.x , self.y , 35 , 65)
     def move(self):
+        zidovi = [
+            pygame.Rect(165, 120, 5, 250),
+            pygame.Rect(165, 370, 120, 5),
+            pygame.Rect(165, 120, 260, 5),
+            pygame.Rect(415, 120, 5, 250),
+        ]
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.y -= self.speed
-        if keys[pygame.K_s]:
-            self.y += self.speed
-        if keys[pygame.K_d]:
-            self.x += self.speed
-            self.pravac = "desno"
-        if keys[pygame.K_a]:
-            self.x -= self.speed
-            self.pravac = "levo"
-        if(self.pravac == "desno"):
-            self.picture = self.txt_player_right
-        if(self.pravac == "levo"):
-            self.picture = self.txt_player_left
+        if False:
+            pass
+        else:
+        
+            self.rect.x = self.x
+            self.rect.y = self.y
+            if keys[pygame.K_w]:
+                clear = 0
+                for i in range(4):
+                    zid = zidovi[i]
+                    if pygame.Rect(self.rect.x,self.rect.y-self.speed,self.rect.width,self.rect.height).colliderect(zid) == False:
+                        clear+=1
+                if clear==4:
+                    self.y-=self.speed
+                
+            if keys[pygame.K_s]:
+                clear = 0
+                for i in range(4):
+                    zid = zidovi[i]
+                    if pygame.Rect(self.rect.x, self.rect.y + self.speed , self.rect.width , self.rect.height).colliderect(zid) == False:
+                        clear +=1
+                if clear == 4:
+
+                    self.y += self.speed
+                
+            if keys[pygame.K_d]:
+                clear = 0
+                for i in range(4):
+                    zid = zidovi[i]
+                    if pygame.Rect(self.rect.x + self.speed , self.rect.y, self.rect.width , self.rect.height).colliderect(zid) == False:
+                        clear +=1
+                if clear == 4:
+
+                    self.x += self.speed
+                
+                self.pravac = "desno"
+            if keys[pygame.K_a]:
+                clear = 0
+                for i in range(4):
+                    zid = zidovi[i]
+                    if pygame.Rect(self.rect.x -self.speed , self.rect.y , self.rect.width , self.rect.height).colliderect(zid) == False:
+                        clear +=1
+                if clear == 4:
+
+                    self.x -= self.speed
+                
+                self.pravac = "levo"
+            if(self.pravac == "desno"):
+                self.picture = self.txt_player_right
+            if(self.pravac == "levo"):
+                self.picture = self.txt_player_left
     def draw(self , surface):
         surface.blit(self.picture , (self.x , self.y))
 
--- game.py
+++ game.py
@@ -6,17 +6,37 @@ prozor = pygame.display.set_mode((1280, 720))
 sat = pygame.time.Clock()
 txt_trava = pygame.image.load("images\\grass.png")
 txt_kuca = pygame.image.load("images\\kuca.png")
+txt_house_floor = pygame.image.load("images\\housefloor.png")
 txt_kuca = pygame.transform.scale(
     txt_kuca, (txt_kuca.get_width() * 0.69, txt_kuca.get_height() * 0.69)
 )
 
 
+zidovi = [
+    pygame.Rect(165, 120, 5, 250),
+    pygame.Rect(165, 370, 120, 5),
+    pygame.Rect(165, 120, 260, 5),
+    pygame.Rect(415, 120, 5, 250),
+]
+
+
 def nacrtaj_mapu():
+
+    prozor.fill("green")
+    
+    """
     for i in range(3):
         for j in range(4):
             prozor.blit(txt_trava, (j * 320, i * 320))
+    
+
+    """
     prozor.blit(txt_kuca, (120, 60))
     prozor.blit(txt_kuca, (500, 60))
+    for zid in zidovi:
+        pygame.draw.rect(prozor, pygame.Color("red"), zid)
+        player.collision = pygame.Rect.colliderect(player.picture.get_rect(), zid)
+
 
 
 def game():
@@ -31,5 +51,8 @@ def game():
         player.draw(prozor)
         pygame.display.update()
         
+        sat.tick(60)
+        print(sat)
+
 
 game()
